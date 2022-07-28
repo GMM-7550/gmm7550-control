87d29c21bb7d763302177d8fe93fc533f1f8be03
Anton Kuzmin <anton.kuzmin@cs.fau.de>
2022-07-28 00:39:50 +0000

    Generate proper i2c transactions to access registers

--- gmm7550/cdce6214.py
+++ gmm7550/cdce6214.py
@@ -1,3 +1,6 @@
+
+from gmm7550.sem_smbus import i2c_msg
+
 class CDCE6214:
 
     def __init__(self, bus, addr=0x68):
@@ -5,8 +8,17 @@ class CDCE6214:
         self.addr = addr
 
     def read_reg(self, reg):
-        return self.bus.read_word_data(self.addr, reg)
+        ma = i2c_msg.write(self.addr, [(reg >> 8) & 0xff, reg & 0xff])
+        md = i2c_msg.read(self.addr, 2)
+        self.bus.acquire()
+        self.bus.i2c_rdwr(ma, md)
+        self.bus.release()
+        d = list(md)
+        return ((d[0]<<8) | d[1])
 
     def write_reg(self, reg, data):
-        self.bus.write_word_data(self.addr, reg, data)
-
+        m = i2c_msg.write(self.addr, [ (reg >> 8) & 0xff,  reg & 0xff,
+                                      (data >> 8) & 0xff, data & 0xff ])
+        self.bus.acquire()
+        self.bus.i2c_rdwr(m)
+        self.bus.release()
